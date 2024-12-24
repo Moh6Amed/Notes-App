f104b8bc8225b12069a4801d3e90d408080b3880
Mohamed <m.khames.66666@gmail.com>
2024-12-24 03:53:50 +0200

    Creating note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,11 @@
+class NoteModel {
+  final String title;
+  final String supTitle;
+  final int color;
+  final String date;
+  NoteModel(
+      {required this.title,
+      required this.supTitle,
+      required this.color,
+      required this.date});
+}
