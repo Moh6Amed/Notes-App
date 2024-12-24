17e46186988abf806ce473f695b501d04eeb02d6
Mohamed <m.khames.66666@gmail.com>
2024-12-24 02:21:43 +0200

    Create edit note view

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,56 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:notesapp/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    'Flutter tips',
-                    style: TextStyle(fontSize: 30, color: Colors.black),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.delete),
-                    iconSize: 28,
-                    color: Colors.black,
-                  ),
-                ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return const EditNoteView();
+          }),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: const Color(0xffFFCC80),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Flutter tips',
+                      style: TextStyle(fontSize: 30, color: Colors.black),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.delete),
+                      iconSize: 28,
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8, bottom: 8),
-              child: Text(
-                'Build your career with mohamed',
-                style:
-                    TextStyle(fontSize: 18, color: Colors.black.withAlpha(160)),
+              Padding(
+                padding: const EdgeInsets.only(top: 8, bottom: 8),
+                child: Text(
+                  'Build your career with mohamed',
+                  style: TextStyle(
+                      fontSize: 18, color: Colors.black.withAlpha(160)),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 12),
-              child: Text(
-                'December 22,2024',
-                style: TextStyle(color: Colors.black.withAlpha(160)),
+              Padding(
+                padding: const EdgeInsets.only(right: 12),
+                child: Text(
+                  'December 22,2024',
+                  style: TextStyle(color: Colors.black.withAlpha(160)),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
